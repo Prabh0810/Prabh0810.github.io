dc0e1cc7f48e02793f9168847b199d050e1cb1a9
Prabh0810 <prabhjotkaur61@dcmail.ca>
2023-11-21 18:02:16 -0500

    insert some data
    - insert images
    - set loop for images
    - set alternative text for images

--- assignment4b/main.js
+++ assignment4b/main.js
@@ -5,10 +5,24 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imagesnames  = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg','pic5.jpg']; 
 
 /* Declaring the alternative text for each image file */
+const alttext = ['Closeup of a blue human eye', 'A scenic landscape', 'Abstract art', 'City skyline at night', 'Colorful flowers'];
 
 /* Looping through images */
+for (let i = 1; i <= imageFilenames.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    newImage.setAttribute('alt', altTexts[i - 1]);
+    thumbBar.appendChild(newImage);
+
+    newImage.addEventListener('click', function() {
+        displayedImage.src = this.src;
+        displayedImage.alt = this.alt;
+      });
+    }
+    
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
