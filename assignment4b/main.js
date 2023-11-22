5f4733a132eb6b0bad7af83791931edb7b603870
Prabh0810 <prabhjotkaur61@dcmail.ca>
2023-11-22 15:30:46 -0500

    adding html validations

--- assignment4b/main.js
+++ assignment4b/main.js
@@ -8,32 +8,37 @@ const overlay = document.querySelector('.overlay');
 const imagenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-const altText = ['Closeup of a blue human eye', 'A scenic landscape', 'Abstract art', 'City skyline at night', 'Colorful flowers'];
+const alts = {
+    'pic1.jpg' : 'Closeup of a human eye',
+    'pic2.jpg' : 'Rock that looks like a wave',
+    'pic3.jpg' : 'Purple and white pansies',
+    'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+    'pic5.jpg' : 'Large moth on a leaf'
+  }
+  
 
 /* Looping through images */
-for (let i = 1; i <= imageFilenames.length; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', imageFilenames[i - 1]); // Corrected the array name and index
-    newImage.setAttribute('alt', altText[i - 1]);
-    thumbBar.appendChild(newImage);
-
-    newImage.addEventListener('click', function() {
-        displayedImage.src = this.src;
-        displayedImage.alt = this.alt;
-    });
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
 }
 
 /* Wiring up the Darken/Lighten button */
-btn.addEventListener('click', function() {
+btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
-
     if (btnClass === 'dark') {
-        btn.setAttribute('class', 'light');
-        btn.textContent = 'Lighten';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
     } else {
-        btn.setAttribute('class', 'dark');
-        btn.textContent = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
-});
+  });
--- assignmnet4a/main.js
+++ assignmnet4a/main.js
@@ -2,21 +2,15 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-const storyText =
-  'It was :temperature: outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. :hero: saw the whole thing, but was not surprised — :insertx: weighs :weight:, and it was a hot day.';
-
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = [
-  'spontaneously combusted',
-  'melted into a puddle on the sidewalk',
-  'turned into a slug and crawled away',
-];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
@@ -27,20 +21,22 @@ function result() {
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
 
-  newStory = newStory.replaceAll(':insertx:', xItem);
-  newStory = newStory.replaceAll(':inserty:', yItem);
-  newStory = newStory.replaceAll(':insertz:', zItem);
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
 
-  const heroName = customName.value || 'Bob';
-  newStory = newStory.replaceAll(':hero:', heroName);
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+  }
 
-  if (document.getElementById('uk').checked) {
-    const weight = `${Math.round(300 * 0.0714286)} stone`;
-    const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
     newStory = newStory.replaceAll('300 pounds', weight);
   }
 
   story.textContent = newStory;
-  story.style.display = 'block';
+  story.style.visibility = 'visible';
 }
