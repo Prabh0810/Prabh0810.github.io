c24515d922ba82fba0c6ace8e677be8ae96d99b6
Prabh0810 <prabhjotkaur61@dcmail.ca>
2023-11-22 18:36:48 -0500

    correct tthe pic source

--- assignmnet4a/main.js
+++ assignmnet4a/main.js
@@ -3,7 +3,7 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
@@ -21,20 +21,20 @@ function result() {
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
 
-  newStory = newStory.replaceAll(':insertx:',xItem);
-  newStory = newStory.replaceAll(':inserty:',yItem);
-  newStory = newStory.replaceAll(':insertz:',zItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replaceAll('Bob', name);
+    newStory = newStory.replace(/Bob/g, name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(300*0.0714286)} stone`;
-    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
-    newStory = newStory.replaceAll('94 fahrenheit', temperature);
-    newStory = newStory.replaceAll('300 pounds', weight);
+    const weight = `${Math.round(300 * 0.0714286)} stone`;
+    const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
   story.textContent = newStory;
